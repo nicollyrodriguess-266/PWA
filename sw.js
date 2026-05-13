0096090fe992952d9a00b37cfea1c07b2f6429b0
nicollyrodriguess-266 <nicollyara0@gmail.com>
2026-05-13 20:41:20 -0300

    correções feitas

--- sw.js
+++ sw.js
@@ -1,11 +1,61 @@
-if('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js')
-    .then(function(registration) {
-        console.log('Service Worker registered with scope:', registration.scope);
-    })
-    .catch(function(error) {
-        console.log('Service Worker registration failed:', error);
-    });
-} else {
-    console.log('Service Workers are not supported in this browser.');
+if ("serviceWorker" in navigator) {
+
+  window.addEventListener("load", () => {
+
+    navigator.serviceWorker.register("sw.js")
+      .then((registration) => {
+
+        console.log("SW registrado:", registration);
+
+      })
+      .catch((error) => {
+
+        console.log("Erro:", error);
+
+      });
+
+  });
+
 }
+
+const CACHE_NAME = "cadastro-app-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/app.js",
+  "/manifest.json",
+  "/css/index.css",
+  "/icon-192.png",
+  "/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+
+  event.waitUntil(
+
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+
+        return cache.addAll(urlsToCache);
+
+      })
+
+  );
+
+});
+
+self.addEventListener("fetch", (event) => {
+
+  event.respondWith(
+
+    caches.match(event.request)
+      .then((response) => {
+
+        return response || fetch(event.request);
+
+      })
+
+  );
+
+});
