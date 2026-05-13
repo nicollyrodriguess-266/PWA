361f6dafcf2aaa0a66d6730e4bba72474e4dc350
wagnermarques <wagnerdocri@gmail.com>
2026-05-13 20:05:56 -0300

    arquivos adicionados

---
+++ sw.js
@@ -0,0 +1,11 @@
+if('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js')
+    .then(function(registration) {
+        console.log('Service Worker registered with scope:', registration.scope);
+    })
+    .catch(function(error) {
+        console.log('Service Worker registration failed:', error);
+    });
+} else {
+    console.log('Service Workers are not supported in this browser.');
+}
